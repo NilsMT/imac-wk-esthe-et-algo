346f18a73152d56037ff27eae7b29f794f790edc
Nils Moreau--Thomas <terminatorletrol@gmail.com>
2026-01-12 15:02:24 +0100

    added a blackhole

--- algorithmic_drawing/sketch.js
+++ algorithmic_drawing/sketch.js
@@ -1,7 +1,7 @@
 let width = 640;
 let height = 400;
 
-let mode = 0;
+let mode = 2;
 let btn;
 
 function canvasResize(w, h) {
@@ -15,10 +15,10 @@ function canvasResize(w, h) {
 function setup() {
     createCanvas(width, height);
 
-    btn = createButton("Switch sketch");
+    btn = createButton("Bouton");
 
     btn.mousePressed(() => {
-        mode = mode < 1 ? 1 : 0;
+        mode = mode == 2 ? 0 : mode + 1;
 
         strokeWeight(1);
         background(255);
@@ -28,8 +28,9 @@ function setup() {
 function draw() {
     if (mode == 0) {
         canvasResize(640, 400);
-        colorMode(RGB, 255);
+        colorMode(HSB, 360, 100, 100); //HSB https://fr.wikipedia.org/wiki/Teinte_Saturation_Valeur
         stroke(0);
+        btn.html("Lignes épaisses");
 
         let n = 0; //N=0
         let x = width; // X=Xrsl%
@@ -43,6 +44,11 @@ function draw() {
             x -= d + 10; // X=X-D-10
             y -= d + 10; // Y=Y-D-10
 
+            //Color
+            let hueValue = map(n, 0, height, 0, 360); //map r to hue rotation
+            stroke(hueValue, 100, 100); //set color from hue
+            //
+
             line(n, n, n, y); //To N,N To N,Y
             line(n, y, x, y); //To N,Y To X,Y
             line(x, y, x, n); //To X,Y To X,N
@@ -51,6 +57,7 @@ function draw() {
     } else if (mode == 1) {
         canvasResize(400, 400);
         colorMode(HSB, 360, 100, 100); //HSB https://fr.wikipedia.org/wiki/Teinte_Saturation_Valeur
+        btn.html("Figure 5");
 
         let a = width / 2; //A=Xrsl%/2
         let b = height / 2; //B=Yrsl%/2
@@ -58,6 +65,7 @@ function draw() {
             //For I=1 To 11
             let r = height * 0.7; //R=Yrsl%*0.7
             let rMax = r;
+
             for (let w = Math.PI / 4; w <= 3.6; w += 0.05) {
                 //For W=Pi/4 To 3.6 Step 0.05
 
@@ -73,8 +81,73 @@ function draw() {
                 line(a - x, b + y, a + x, b - y); //Line A-X,B+Y,A+X,B-Y
                 line(a - x, b + y, a + y, b + x); //Line A-X,B+Y,A+Y,B+X
                 line(a + y, b + x, a + x, b - y); //Line A+X,B+X,A+X,B-Y
-                r = r * 0.94; //R=R*0.94
+                r *= 0.94; //R=R*0.94
+            }
+
+            r /= 0.94; //R=R/0.94
+
+            for (let w = 3.6; w >= Math.PI / 4; w -= 0.05) {
+                console.warn("a");
+
+                //Color
+                let hueValue = map(r, 0, rMax, 0, 360); //map r to hue rotation
+                stroke(hueValue, 100, 100); //set color from hue
+                //
+
+                let x = r * Math.cos(w);
+                let y = r * Math.sin(w);
+                line(a + x, b - y, a - y, b - x);
+                line(a - y, b - x, a - x, b + x);
+                line(a - x, b + y, a + x, b - y);
+                line(a - x, b + y, a + y, b + x);
+                line(a + y, b + x, a + x, b - y);
+                r /= 0.94;
             }
         }
+    } else if (mode == 2) {
+        canvasResize(600, 600);
+        colorMode(RGB);
+        btn.html("Improvisation");
+        strokeWeight(1);
+
+        let x = width;
+        let y = height;
+
+        let midX = x / 2;
+        let midY = y / 2;
+
+        let precision = 3000; //more = better precision
+        let radius = min(width, height) * 0.2;
+        let length = 100;
+
+        for (let angle = 0; angle < TWO_PI; angle += TWO_PI / precision) {
+            let endX = midX + Math.cos(angle) * radius;
+            let endY = midY + Math.sin(angle) * radius;
+
+            //blackhole outer layers
+            stroke(255, 255, 0);
+            line(midX, midY, endX, endY); //circle
+            line(
+                endX,
+                endY,
+                endX + Math.sin(angle) * length, //100 for length, trace a line from outer circle based on angle (-1 to 1)
+                endY + Math.cos(angle) * length //100 for length, trace a line from outer circle based on angle (-1 to 1)
+            );
+
+            //blackhole center layers
+            stroke(0);
+            ellipse(midX, midY, radius * 1.5, radius * 1.5);
+            fill(0);
+        }
+
+        //so horizontal line is -pi/4 to pi/4  (2pi/8 = pi/4)
+        strokeWeight(10);
+        stroke(255, 255, 0);
+        line(
+            midX - Math.sin(Math.PI / 4) * length,
+            midY - Math.cos(Math.PI / 4) * length,
+            midX + Math.sin(Math.PI / 4) * length,
+            midY + Math.cos(Math.PI / 4) * length
+        );
     }
 }
